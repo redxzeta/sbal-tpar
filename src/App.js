dca7ba18e7da292b58c13759c5bf04b9e4a2cb74
redxzeta <xzetanzero@gmail.com>
2020-12-13 23:20:42 -0800

    react todo list with scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Todo from "./components/Todo";
 
+// const toDoForm = {
+//   id: "",
+//   completed: false,
+//   task: "",
+//   date: "",
+// };
 export default function App() {
   return (
-    <div>
-      <h1>TEST</h1>
-    </div>
+    <BackgroundContainer>
+      <Todo />
+    </BackgroundContainer>
   );
 }
+
+const BackgroundContainer = ({ children }) => (
+  <div className="todo__background">
+    <div className="container">
+      <div className="paper__background">{children}</div>
+    </div>
+  </div>
+);
---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Todo() {
+  return (
+    <div className="todo__section">
+      <div className="heading-primary">
+        <span className="heading-primary--title">TODO LIST</span>
+      </div>
+      <form>
+        <input type="text" className="todo-input" />
+        <button className="todo--button" type="submit">
+          SUBMIT
+        </button>
+      </form>
+      h
+    </div>
+  );
+}
