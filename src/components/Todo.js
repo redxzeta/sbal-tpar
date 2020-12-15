cd7380c6e9d98f11e0d3a96806493a15479b574a
redxzeta <xzetanzero@gmail.com>
2020-12-14 22:26:40 -0800

    flex box

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,17 @@
-import React from "react";
+import { PencilIcon, TrashcanIcon } from "@primer/octicons-react";
+import React, { useEffect, useState } from "react";
 
 export default function Todo() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((result) => {
+        setTodo(result);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const [todo, setTodo] = useState("");
+  console.log(todo);
   return (
     <div className="todo__section">
       <div className="heading-primary">
@@ -12,7 +23,26 @@ export default function Todo() {
           SUBMIT
         </button>
       </form>
-      <div className="todolist__item">TOW</div>
+      <div className="todolist__container">
+        <div className="todolist__parent">
+          {todo &&
+            todo.map((task) => (
+              <ToDoListItem key={task.id} title={task.title} />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
+
+const ToDoListItem = (props) => (
+  <div className="todolist__item">
+    <div className="todolist__item--start">{props.title}</div>
+    <button className="icon--button">
+      <PencilIcon className="img--button" size={24} />
+    </button>
+    <button className="icon--button">
+      <TrashcanIcon className="img--button" size={24} />
+    </button>
+  </div>
+);
